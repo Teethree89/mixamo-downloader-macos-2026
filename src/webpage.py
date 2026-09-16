67bb0f99684740341b2e9aea5230ee14e7b99011
Ivan Dmitrivich Popov <80078290+Teethree89@users.noreply.github.com>
2026-09-16 13:39:54 -0400

    Port embedded Mixamo browser to PySide6

--- src/webpage.py
+++ src/webpage.py
@@ -1,38 +1,14 @@
 # Third-party modules
-from PySide2 import QtCore, QtWebEngineWidgets, QtWidgets
+from PySide6 import QtCore, QtWebEngineCore
 
 
-class CustomWebPage(QtWebEngineWidgets.QWebEnginePage):
-    """Custom QWebEnginePage that catches data from the JavaScript console.
-
-    This allows us to read variables that are only stored in the browser,
-    such as the 'access_token' used by Mixamo. We'll then use that token
-    as an 'Authentication' header when sending HTTP Requests to its API.
-    """    
+class CustomWebPage(QtWebEngineCore.QWebEnginePage):
+    """Custom QWebEnginePage that captures the Mixamo access token."""
 
     retrieved_token = QtCore.Signal(str)
 
-    def __init__(self, *args, **kwargs):        
-        super().__init__(*args, **kwargs)
-
-        # Reimplement the javaScriptConsoleMessage method.
-        self.javaScriptConsoleMessage = self.handle_console_message
-
-    def handle_console_message(self, level, message, lineNumber, sourceID):
-        """This method decides what to do with console messages.
-
-        :param level: Severity level a JavaScript console message can have
-        :type level: QWebEnginePage.JavaScriptConsoleMessageLevel
-
-        :param message: Message printed to the console
-        :type message: str
-
-        :param lineNumber: Line number where the message was printed
-        :type lineNumber: int
-
-        :param sourceID: Source ID
-        :type sourceID: str
-        """
+    def javaScriptConsoleMessage(self, level, message, line_number, source_id):
         if "ACCESS TOKEN" in message:
             access_token = message.split(":")[-1].strip()
             self.retrieved_token.emit(access_token)
+        super().javaScriptConsoleMessage(level, message, line_number, source_id)
